b8339d886df8559744c3080569e9150c4f939721
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2013-10-20 14:31:04 -0400

    Added basic navigation from main page.

--- src/com/example/usabilityapp/MainActivity.java
+++ src/com/example/usabilityapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.usabilityapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +19,26 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void gotoExercise(View view) {
+	    Intent intent = new Intent(this, ExerciseActivity.class);
+	    startActivity(intent);
+	}
+	
+	public void gotoGoals(View view){
+		Intent intent = new Intent(this, GoalsActivity.class);
+		startActivity(intent);
+	}
+	
+	public void gotoSettings(View view) {
+	    Intent intent = new Intent(this, SettingsActivity.class);
+	    startActivity(intent);
+	}
+	
+	public void gotoSchedule(View view){
+		Intent intent = new Intent(this, ScheduleActivity.class);
+		startActivity(intent);
 	}
 
 }
